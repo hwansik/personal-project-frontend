84c73fcf0b153e597de9156ac827880a2116367c
hwansik <hwansik08@gmail.com>
2023-07-07 18:05:53 +0900

    feat: [민환식] Navigation Bar 추가 시도 [VUEx]

--- HwansikMin/frontend/src/App.vue
+++ HwansikMin/frontend/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <navigation-menu-page/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
+<script>
+import NavigationMenuPage from
+'./views/basics/layout/navigation/NavigationMenuPage.vue'
+
+export default {
+  component: {
+    NavigationMenuPage
+  }
+}
+</script>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +26,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
